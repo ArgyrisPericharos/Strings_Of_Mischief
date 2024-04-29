5d806997051ba46030e273bd1780679a27d3864c
LKBN21 <114396668+LKBN21@users.noreply.github.com>
2024-04-29 19:13:49 +0100

    LevelAlmost

---
+++ Assets/LB_Scripts/LB_GenStop.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LB_GenStop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GeneralPublic"))
+        {
+            other.gameObject.GetComponentInParent<LB_GenPublic>().DisableGoal = true;
+
+        }
+    }
+    /*public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("GeneralPublic"))
+        {
+            other.gameObject.GetComponentInParent<LB_GenPublic>().DisableGoal = false;
+
+        }
+    }*/
+}
